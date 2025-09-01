e06c7fca979725f115b07b4f60ca825c93a288bb
ATTHAPOL\Atthapol <Atthapol>
2025-09-01 10:19:37 +0700

    update ui

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -57,7 +57,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="relative min-h-screen w-full flex items-center justify-center bg-gray-500 overflow-hidden">
+    <div className="relative min-h-screen w-full flex items-center justify-center bg-gray-400 overflow-hidden">
       {/* === Red Top-Right Blob === */}
       <svg
         className="absolute top-0 right-0 w-full h-full z-0"
@@ -74,7 +74,7 @@ export default function LoginPage() {
       {/* === Concentric Circles Bottom Left === */}
       <div className="absolute bottom-[-180px] left-[-180px] w-[500px] h-[500px] rounded-full bg-[#b92626] flex items-center justify-center">
         <div className="w-[400px] h-[400px] rounded-full bg-white flex items-center justify-center">
-          <div className="w-[300px] h-[300px] rounded-full bg-gray-500"></div>
+          <div className="w-[300px] h-[300px] rounded-full bg-gray-400"></div>
         </div>
       </div>
 
@@ -83,16 +83,18 @@ export default function LoginPage() {
         <div className="bg-white/100 backdrop-blur-sm p-8 pt-24 rounded-2xl shadow-2xl">
           <Logo />
           <div className="text-center mb-8">
-            <h1 
-                className={`${prompt.className} text-center text-2xl font-bold 
-              bg-gradient-to-r from-red-500 via-gray-800 to-red-500 
-              bg-clip-text text-transparent animate-gradient-x`}
+            <h1
+              className={`${prompt.className} text-center text-2xl font-bold`}
             >
-                CS Center
+              ระบบ <span className="text-[#e51717]">CS ONE</span>
             </h1>
-            <p className="text-gray-600">
-              {/* CS One DataCenter */}
-            </p>
+
+            <h1 className={`text-center text-1xl font-bold mt-2`}>
+              Smart Crop Smart Solutions
+            </h1>
+            {/* <p className="text-gray-800">
+              Smart Crop Smart Solutions
+            </p> */}
           </div>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
             <div className="my-4">
@@ -146,14 +148,14 @@ export default function LoginPage() {
                 />
                 <label
                   htmlFor="remember-me"
-                   className={`${prompt.className} ml-2 block text-gray-600`}
+                  className={`${prompt.className} ml-2 block text-gray-600`}
                 >
                   บันทึกรหัส
                 </label>
               </div>
               <Link
                 href="#"
-                 className={`${prompt.className} text-gray-600 hover:text-gray-900`}
+                className={`${prompt.className} text-gray-600 hover:text-gray-900`}
               >
                 ลืมรหัสผ่าน
               </Link>
@@ -165,7 +167,7 @@ export default function LoginPage() {
                 className="font-serif w-fit flex items-center justify-center py-2 px-6 text-white bg-gray-500 rounded-xl hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-transform transform hover:scale-105 disabled:bg-gray-400 mt-4"
               >
                 {isSubmitting ? "กำลังเข้าสู่ระบบ..." : "เข้าสู่ระบบ"}
-                {!isSubmitting && <ArrowRight className="ml-2" size={20} />}
+                {/* {!isSubmitting && <ArrowRight className="ml-2" size={20} />} */}
               </button>
             </div>
           </form>
